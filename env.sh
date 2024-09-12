bb40a709391c97302c5269acaae36547ed9460e0
pablo-flores <49452629+pablo-flores@users.noreply.github.com>
2024-09-12 22:28:12 +0000

    ajuste de test

---
+++ env.sh
@@ -0,0 +1,6 @@
+env MONGO_PASS=omusr2022
+env MONGO_USER=omusr
+env MONGODB_COLLECTION=alarm
+env MONGODB_DATABASENAME=OutageManager
+env MONGODB_URI=mongodb://omusr:omusr2022@ulmongorouapp1.hor.corp.cloudteco.com.ar:27017/OutageManager
+env TZ=America/Argentina/Buenos_Aires
