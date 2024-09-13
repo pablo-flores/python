e1b1afab64e8f1d659afb7cca32a3fcf149d50de
pablo-flores <pablo.flores@live.com.ar>
2024-09-13 06:07:06 +0100

    primer pedido de Gustavo

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, jsonify
 from flask_pymongo import PyMongo
 from datetime import datetime
 from pytz import timezone, utc
+import pandas as pd
 import os
 
 app = Flask(__name__, template_folder="templates")
@@ -13,37 +14,29 @@ username = os.getenv('MONGO_USER')
 password = os.getenv('MONGO_PASS')
 hostmongodb = os.getenv('MONGODB_URI')
 
-# Reemplazar los marcadores de posición en la cadena
-#hostmongodb = hostmongodb.replace('${MONGO_USER}', username)
-#hostmongodb = hostmongodb.replace('${MONGO_PASS}', password)
-
-# Verificar que las variables no sean None
+# Verificar las variables de entorno
 if username and password and hostmongodb:
     hostmongodb = hostmongodb.replace('${MONGO_USER}', username).replace('${MONGO_PASS}', password)
 else:
-    #raise ValueError("Las variables de entorno MONGODB_URI o MONGO_USER o MONGO_PASS no están definidas.")
     print("Las variables de entorno MONGODB_URI o MONGO_USER o MONGO_PASS no están definidas.")
 
 # Configuración de la conexión con MongoDB
-app.config["MONGO_URI"] = hostmongodb 
+app.config["MONGO_URI"] = hostmongodb
 mongo = PyMongo(app)
-
-
-def format_datetime(dt):
-    if isinstance(dt, datetime):
-        return dt.strftime('%d-%m-%Y %H:%M:%S')
-    return None
 
 # Definir la zona horaria Local (Buenos Aires)
 buenos_aires_tz = timezone('America/Argentina/Buenos_Aires')
 
-
-
+# Ruta principal que carga la página
 @app.route('/')
 def index():
-    # Consulta MongoDB con proyección y filtros específicos
+    return render_template('viewTop10.html')
+
+# Nueva ruta para obtener las alarmas en formato JSON
+@app.route('/get_alarmas', methods=['GET'])
+def get_alarmas():
     cursor = mongo.db.alarm.find(
-        {"alarmState": {"$in": ['RAISED', 'UPDATED', 'RETRY']}},
+        {"alarmState": {"$in": ['RAISED', 'UPDATED', 'RETRY', 'CLEARED']}},
         {
             "_id": 0,
             "alarmId": 1,
@@ -53,30 +46,82 @@ def index():
             "TypeNetworkElement": "$networkElement.type",
             "networkElementId": 1,
             "timeResolution": 1,
+            "sourceSystemId": 1,
+            "origenId": 1,
             "inicioOUM": "$omArrivalTimestamp",
-            "alarmRaisedTime": 1
+            "alarmRaisedTime": 1,
+            "alarmClearedTime": 1
         }
-    ).sort("clients", -1).limit(10)
-    
+    ).sort("_id", -1)
+
     alarmas = []
     for alarma in cursor:
-        alarma['inicioOUM'] = alarma.get('inicioOUM').replace(tzinfo=utc).astimezone(buenos_aires_tz).strftime('%d-%m-%Y %H:%M:%S')
-        alarma['alarmRaisedTime'] = alarma.get('alarmRaisedTime').replace(tzinfo=utc).astimezone(buenos_aires_tz).strftime('%d-%m-%Y %H:%M:%S')
-        alarmas.append(alarma)
-    
-    # Fecha de última actualización
-    fecha_actualizacion = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+        if alarma.get('inicioOUM'):
+            alarma['inicioOUM'] = alarma.get('inicioOUM').replace(tzinfo=utc).astimezone(buenos_aires_tz).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            alarma['inicioOUM'] = '-'
 
-    
-  # Pasamos los resultados y la fecha de actualización a la plantilla HTML
-    return render_template('viewTop10.html', alarmas=alarmas, fecha_actualizacion=fecha_actualizacion)
+        if alarma.get('alarmRaisedTime'):
+            alarma['alarmRaisedTime'] = alarma.get('alarmRaisedTime').replace(tzinfo=utc).astimezone(buenos_aires_tz).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            alarma['alarmRaisedTime'] = '-'
+
+        if alarma.get('alarmClearedTime'):
+            alarma['alarmClearedTime'] = alarma.get('alarmClearedTime').replace(tzinfo=utc).astimezone(buenos_aires_tz).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            alarma['alarmClearedTime'] = '-'
+
+        if not alarma.get('timeResolution'):
+            alarma['timeResolution'] = '-'            
+
+        alarmas.append(alarma)
+
+    return jsonify({"alarmas": alarmas})
+
+# Ruta para exportar los datos
+@app.route('/export/<format>')
+def export_data(format):
+    cursor = mongo.db.alarm.find(
+        {"alarmState": {"$in": ['RAISED', 'UPDATED', 'RETRY', 'CLEARED']}},
+        {   "_id": 0,
+            "alarmId": 1, "alarmState": 1, "alarmType": 1, 
+            "inicioOUM": "$omArrivalTimestamp", "alarmRaisedTime": 1, "alarmClearedTime": 1,              
+            "TypeNetworkElement": "$networkElement.type", "networkElementId": 1, "clients": 1,
+            "timeResolution": 1, "sourceSystemId": 1, "origenId": 1            
+        }
+    ).sort("_id", -1)
+
+    alarmas = list(cursor)
+    df = pd.DataFrame(alarmas)
+
+    # Reordenar las columnas
+    df = df[['alarmId', 'alarmState', 'alarmType', 'inicioOUM', 'alarmRaisedTime', 'alarmClearedTime', 
+             'TypeNetworkElement', 'networkElementId', 'clients', 'timeResolution', 'sourceSystemId', 'origenId']]
+
+    fecha_actual = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    if format == 'csv':
+        csv_data = df.to_csv(index=False)
+        return Response(csv_data, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=SIA_alarmas_"+fecha_actual+".csv"})
+    elif format == 'excel':
+        output = pd.ExcelWriter('/tmp/alarmas.xlsx', engine='xlsxwriter')
+        df.to_excel(output, index=False)
+        output.close()
+
+        with open('/tmp/alarmas.xlsx', 'rb') as f:
+            excel_data = f.read()
+        return Response(excel_data, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        headers={"Content-disposition": "attachment; filename=SIA_alarmas_"+fecha_actual+".xlsx"})
+    else:
+        return "Unsupported format. Please use 'csv' or 'excel'."
 
 #if __name__ == '__main__':
-#    app.run(debug=True)
+#    port = os.environ.get('FLASK_PORT') or 8080
+#    port = int(port)
+#    app.run(port=port, host='0.0.0.0')
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')    
-
+    app.run(port=int(port), host='0.0.0.0')
+# se levanta con
+# waitress-serve --listen=0.0.0.0:8081 app:app    
